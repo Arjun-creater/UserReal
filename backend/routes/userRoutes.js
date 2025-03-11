9419f810fe2f7cf92133fbe35ddc65e4dec7b4a7
Arjun-creater <155612773+Arjun-creater@users.noreply.github.com>
2025-03-11 16:15:29 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { getUsers, createUser, updateUser, deleteUser } from '../controllers/userController.js';
+import { validateNewUser, validateUserUpdate } from '../middleware/validateUser.js'; 
 
 const router = express.Router();
 
 router.get('/', getUsers);
-router.post('/', createUser);
-router.put('/:id', updateUser);
+router.post('/', validateNewUser, createUser);
+router.put('/:id', validateUserUpdate, updateUser);
 router.delete('/:id', deleteUser);
 
-export default router; 
+export default router;
